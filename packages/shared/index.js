77db5272758a178f8300e9a36583238e1c9327fc
ChikhM1948 <chikh@cuniv-naama.dz>
2026-09-23 17:16:43 +0100

    Prepare API deployment on Render

---
+++ packages/shared/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// Types partagés entre apps/api, apps/web et apps/desktop.
+// Les montants monétaires sont TOUJOURS en centimes entiers (integer), jamais en float.
+Object.defineProperty(exports, "__esModule", { value: true });
